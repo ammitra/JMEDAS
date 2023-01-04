60aedccc0b7c3501526278387fd6076f3daf04a9
Amitav Mitra <amitra3@jhu.edu>
2023-01-04 14:56:28 -0600

    notebook -> script

---
+++ exercises/ex1p1.py
@@ -0,0 +1,30 @@
+import ROOT
+f = ROOT.TFile("$CMSSW_BASE/src/Analysis/JMEDAS/notebooks/files/ttjets.root")
+
+h_ptAK4   = f.Get("h_ptAK4")
+h_etaAK4  = f.Get("h_etaAK4")
+h_phiAK4  = f.Get("h_phiAK4")
+h_mAK4    = f.Get("h_mAK4")
+
+c = ROOT.TCanvas('c', 'c', 800, 600)
+
+c.Divide(2,2)
+c.cd(1)
+ROOT.gPad.SetLogy()
+h_ptAK4.Draw()
+h_ptAK4.GetXaxis().SetRangeUser(0, 1000)
+c.cd(2)
+h_etaAK4.Draw()
+c.cd(3)
+h_phiAK4.Draw()
+h_phiAK4.SetMinimum(0)
+c.cd(4)
+ROOT.gPad.SetLogy()
+h_mAK4.Draw()
+h_mAK4.GetXaxis().SetRangeUser(0, 200)
+ROOT.gPad.SetLogy()
+
+#ROOT.enableJSVis()
+c.Draw()
+c.Print('plots1.png', 'png')
+c.Print('plots1.pdf', 'pdf')
